9aa36521c6d51a77b61c843793ff41afb01b0234
nTrong <55505553+ntrong22@users.noreply.github.com>
2022-05-09 19:56:07 +0700

    first commit

---
+++ Imagecompression.py
@@ -0,0 +1,29 @@
+#tách màu
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+
+img = mpimg.imread('KPDL/b.jpg')
+plt.imshow(img)
+imgplot = plt.imshow(img)
+plt.axis('off')
+plt.show() 
+
+X = img.reshape((img.shape[0]*img.shape[1], img.shape[2]))
+#ctrl k c, u
+
+for K in [3]:
+    kmeans = KMeans(n_clusters=K).fit(X)
+    label = kmeans.predict(X)
+
+    img4 = np.zeros_like(X)
+    # replace each pixel by its center
+    for k in range(K):
+        img4[label == k] = kmeans.cluster_centers_[k]
+    # reshape and display output image
+    img5 = img4.reshape((img.shape[0], img.shape[1], img.shape[2]))
+    plt.imshow(img5, interpolation='nearest')
+    plt.axis('off')
+    plt.show()
